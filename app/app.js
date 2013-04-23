d8042335741de03208818c455f7eb91e583517d8
Maxime Cony <maxime.cony@gmail.com>
2013-04-22 21:44:24 -0700

    Add model

--- app/app.js
+++ app/app.js
@@ -1,15 +1,15 @@
 $(function(){
 
+  //Create Task model
 	var Task = Backbone.Model.extend({
 
     defaults: function() {
       return {
         title: "new task",
-        order: Todos.nextOrder(),
         done: false
       };
     },
-
+    //Make the task done or not
     toggle: function() {
       this.save({done: !this.get("done")});
     }

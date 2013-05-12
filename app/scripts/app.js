2942d5c1536441519a51867d758bb7b11b613024
Maxime Cony <maxime.cony@gmail.com>
2013-05-12 14:56:10 -0700

    solve edit problem on importance radio

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,56 +1,58 @@
 $(function(){
 
-    // Create a model for the tasks
-    var Task = Backbone.Model.extend({
+    var App = {
+        Models: {},
+        Views: {},
+        Collections: {}
+    }
 
-        // Will contain three attributes.
-        // These are their default values
-
+    /* 
+    * Task Model
+    */
+    App.Models.Task = Backbone.Model.extend({
+        // Will contain default attributes.
         defaults:{
             title: 'New task',
             checked: false,
             importance: 0
         },
-
         // Helper function for checking/unchecking a task
         toggle: function(){
             this.save('checked', !this.get('checked'));
         }
     });
 
-    // Create a collection of tasks
-    var TaskList = Backbone.Collection.extend({
-
+    /* 
+    * Task Collection
+    */
+    App.Collections.Tasks = Backbone.Collection.extend({
         // Will hold objects of the Task model
-        model: Task,
-
+        model: App.Models.Task,
         // Set up the local storage
         localStorage: new Backbone.LocalStorage("todolistApp"),
-
         // Return an array only with the checked tasks
         getChecked: function(){
             return this.where({checked:true});
         }
     });
 
-    // This view turns a Task model into HTML. Will create LI elements.
-    var TaskView = Backbone.View.extend({
+
+    /* 
+    * Task View
+    */
+    App.Views.Task = Backbone.View.extend({
         tagName: 'div',
         className: 'alert',
-        
         template: _.template($('#taskTemplate').html()),
-
         events:{
             'click .taskCheckbox': 'toggleTask',
             'click .removeTask': 'removeTask',
-            "keypress .taskEdit"  : "taskInputKeypress",
+            'click .closeEditMode': 'closeEditMode',
+            "submit .editTaskForm"  : "editTask",
             "dblclick"  : "editMode"
         },
 
         initialize: function(){
-
-            // Set up event listeners. The change backbone event
-            // is raised when a property changes (like the checked field)
             this.listenTo(this.model, 'change', this.render);
             this.listenTo(this.model, 'destroy', this.remove);
         },
@@ -62,21 +64,15 @@ $(function(){
             if(this.model.get('checked')){
                 className = 'alert alert-success';
             } else {
-
                 var classNames = ["alert alert-info", "alert alert-warning", "alert alert-error"];
                 className = classNames[this.model.get('importance')];
             }
-
             // Create the HTML
-            this.$el.attr('class', className).html(this.template(this.model.toJSON()));
+            this.$el.attr('class', className)
+            .html(this.template(this.model.toJSON()))
+            .find('input:radio[name=importance]:eq(' + this.model.get('importance') + ')')
+            .prop('checked', true);
 
-            this.$('input').prop('checked', this.model.get('checked'));
-
-            // Keep the edit input in the task object
-            this.input = this.$('.taskEdit');
-
-            // Returning the object is a good practice
-            // that makes chaining possible
             return this;
         },
 
@@ -100,35 +96,26 @@ $(function(){
             this.$el.removeClass('editMode');
         },
 
-        taskInputKeypress: function(e) {
+        editTask: function(e) {
 
-            // on press Enter
-            if (e.keyCode == 13) {
-                
-                var title = this.input.val();
-                // Prevent empty validation
-                if (!title) return;
+            e.preventDefault();
 
-                // Edit the task
-                tasks.create({
-                    title: this.input.val(),
-                    importance: $('input[name=importance]:checked').val()
-                }); 
-                this.closeEditMode();
-            }
-
-            // on press Escape
-            if (e.keyCode == 27) {
-                
-                this.closeEditMode();
-            }            
-
+            var title = this.$('.taskTitleEditInput').val();
+            // Prevent empty validation
+            if (!title) return;
+            // Edit the task
+            this.model.save({
+                title: title,
+                importance: this.$el.find('input[name=importance]:checked').val()
+            }); 
+            this.closeEditMode();           
         }
-
     });
 
-
-    var TaskListView = Backbone.View.extend({
+    /* 
+    * Tasks View
+    */
+    App.Views.Tasks = Backbone.View.extend({
 
         tagName: 'div',
 
@@ -136,13 +123,13 @@ $(function(){
 
         initialize: function(){
             //listen the add event
-            this.collection.on('add', this.addOne, this);
+            this.listenTo(this.collection, 'add', this.addOne);
         },
 
         addOne: function(model){
 
             //create a new collection view
-            var taskView = new TaskView({model: model});
+            var taskView = new App.Views.Task({model: model});
             //render the collection
             this.$el.prepend(taskView.render().el);
         },
@@ -154,8 +141,9 @@ $(function(){
         }
     });
 
-    // The main view of the application
-    var App = Backbone.View.extend({
+    /* 
+    * Tasks View
+    */ App.Views.App = Backbone.View.extend({
 
         // Base the view on an existing element
         el: $('#app'),
@@ -166,8 +154,8 @@ $(function(){
 
         initialize: function(){
 
-            this.tasks = new TaskList();
-            var taskListView = new TaskListView({collection: this.tasks});
+            this.tasks = new App.Collections.Tasks();
+            var taskListView = new App.Views.Tasks({collection: this.tasks});
             this.tasks.fetch();
         },
 
@@ -188,6 +176,6 @@ $(function(){
         },
     });
 
-    new App();
+    var app = new App.Views.App();
 
 });
